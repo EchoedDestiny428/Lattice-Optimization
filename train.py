76bfeecf6c12b51c52adfd2f7f814900bf50dc3f
echoeddestiny428 <zacharyng428@gmail.com>
2026-06-15 12:56:09 +0800

    fliter by neighbors

--- train.py
+++ train.py
@@ -77,10 +77,12 @@ def stl_to_voxel_tensor(stl_path, grid_size=64, save_debug_h5=None):
         chunk = scaled_indices[i : i + chunk_size]
         stl_matrix[chunk[:, 0], chunk[:, 1], chunk[:, 2]] = 1.0
 
-    stl_matrix = (stl_matrix 
-                  * stl_matrix[np.r_[0, 0:63], :, :] * stl_matrix[np.r_[1:64, 63], :, :] 
-                  * stl_matrix[:, np.r_[0, 0:63], :] * stl_matrix[:, np.r_[1:64, 63], :] 
-                  * stl_matrix[:, :, np.r_[0, 0:63]] * stl_matrix[:, :, np.r_[1:64, 63]])
+    horizontal_neighbors = (
+        stl_matrix[np.r_[0, 0:63], :, :] + stl_matrix[np.r_[1:64, 63], :, :] +
+        stl_matrix[:, np.r_[0, 0:63], :] + stl_matrix[:, np.r_[1:64, 63], :]
+    )
+
+    stl_matrix = ((stl_matrix == 1.0) & (horizontal_neighbors >= 4)).astype(np.float32)
         
     print(f"[INFO] Voxelization complete. Total Solid Voxels Detected: {int(np.sum(stl_matrix))}")
     
