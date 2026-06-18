d4d694a2a8a14538dc97eb2c016b0a93761e0b1c
echoeddestiny428 <zacharyng428@gmail.com>
2026-06-18 09:53:46 +0800

    change to make a subdir for each sample

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import trimesh
+import json
 
 OUTPUT_DIR = os.path.join("data", "generated_stl")
 os.makedirs(OUTPUT_DIR, exist_ok=True)
@@ -49,25 +50,82 @@ def generate_density_matched_lattice(params, target_density, tolerance=0.01):
 
 amount = 10
 
+DATASET_DIR = os.path.join("data", "samples")
+os.makedirs(DATASET_DIR, exist_ok=True)
+
 for i in range(amount):
-    filename = f"math_lattice_{i:03d}.stl"
-    filepath = os.path.join(OUTPUT_DIR, filename)
-    
+
+    sample_dir = os.path.join(
+        DATASET_DIR,
+        f"sample_{i:06d}"
+    )
+    os.makedirs(sample_dir, exist_ok=True)
+
     c1 = np.random.uniform(-1.5, 1.5)
     c2 = np.random.uniform(-1.5, 1.5)
     c3 = np.random.uniform(-1.5, 1.5)
     c4 = np.random.uniform(-1.0, 1.0)
     c5 = np.random.uniform(-0.5, 0.5)
-    
-    if abs(c1) + abs(c2) + abs(c3) + abs(c4) + abs(c5) < 0.2:
-        c1 = 1.0
-        
-    params = [c1, c2, c3, c4, c5]
-    print(f" {i:03d}/{amount} -> { [round(p,2) for p in params] }")
-    
-    mesh = generate_density_matched_lattice(params, target_density=TARGET_DENSITY)
-    
-    mesh.process(validate=True)
-    mesh.export(filepath)
 
-print(f"\nDone -> '{OUTPUT_DIR}'.")
+    if abs(c1)+abs(c2)+abs(c3)+abs(c4)+abs(c5) < 0.2:
+        c1 = 1.0
+
+    params = [c1, c2, c3, c4, c5]
+
+    mesh = generate_density_matched_lattice(
+        params,
+        target_density=TARGET_DENSITY
+    )
+
+    mesh.process(validate=True)
+
+    # ------------------
+    # Save STL
+    # ------------------
+
+    stl_path = os.path.join(
+        sample_dir,
+        "lattice.stl"
+    )
+
+    mesh.export(stl_path)
+
+    # ------------------
+    # Metadata
+    # ------------------
+
+    metadata = {
+        "parameters": {
+            "c1": float(c1),
+            "c2": float(c2),
+            "c3": float(c3),
+            "c4": float(c4),
+            "c5": float(c5)
+        },
+        "target_density": TARGET_DENSITY,
+        "box_size": BOX_SIZE
+    }
+
+    with open(
+        os.path.join(sample_dir, "metadata.json"),
+        "w"
+    ) as f:
+        json.dump(metadata, f, indent=4)
+
+    # ------------------
+    # Labels (empty for now)
+    # ------------------
+
+    labels = {
+        "relative_density": None,
+        "effective_modulus": None,
+        "peak_force": None,
+        "ea": None,
+        "sea": None
+    }
+
+    with open(
+        os.path.join(sample_dir, "labels.json"),
+        "w"
+    ) as f:
+        json.dump(labels, f, indent=4)
