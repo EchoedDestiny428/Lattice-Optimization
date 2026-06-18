d89bda43fd782a2268acdcabfc9cd05a8d549a41
echoeddestiny428 <zacharyng428@gmail.com>
2026-06-18 15:59:27 +0800

    more debug

--- mapdlgen.py
+++ mapdlgen.py
@@ -1,4 +1,3 @@
-import numpy as np
 from ansys.mapdl.core import launch_mapdl
 
 BOX_SIZE = 10.0
@@ -9,13 +8,21 @@ def voxels_to_mapdl(voxels):
     nx, ny, nz = voxels.shape
     dx = BOX_SIZE / nx
 
+    print("Launching MAPDL...")
+
     mapdl = launch_mapdl()
+
     mapdl.clear()
     mapdl.prep7()
 
+    print("Defining element/material...")
+
     mapdl.et(1, 185)
+
     mapdl.mp("EX", 1, 200e9)
     mapdl.mp("NUXY", 1, 0.3)
+
+    print("Creating nodes...")
 
     node_map = {}
     nid = 1
@@ -23,28 +30,38 @@ def voxels_to_mapdl(voxels):
     for i in range(nx + 1):
         for j in range(ny + 1):
             for k in range(nz + 1):
-                mapdl.n(nid, i*dx, j*dx, k*dx)
+
+                mapdl.n(nid, i * dx, j * dx, k * dx)
+
                 node_map[(i, j, k)] = nid
                 nid += 1
+
+    print("Creating elements...")
+
+    elem_count = 0
 
     for i in range(nx):
         for j in range(ny):
             for k in range(nz):
 
-                if voxels[i, j, k] == 0:
+                if not voxels[i, j, k]:
                     continue
 
                 n = node_map
 
                 mapdl.e(
                     n[(i, j, k)],
-                    n[(i+1, j, k)],
-                    n[(i+1, j+1, k)],
-                    n[(i, j+1, k)],
-                    n[(i, j, k+1)],
-                    n[(i+1, j, k+1)],
-                    n[(i+1, j+1, k+1)],
-                    n[(i, j+1, k+1)],
+                    n[(i + 1, j, k)],
+                    n[(i + 1, j + 1, k)],
+                    n[(i, j + 1, k)],
+                    n[(i, j, k + 1)],
+                    n[(i + 1, j, k + 1)],
+                    n[(i + 1, j + 1, k + 1)],
+                    n[(i, j + 1, k + 1)],
                 )
+
+                elem_count += 1
+
+    print(f"Created {elem_count} elements")
 
     return mapdl
--- sim.py
+++ sim.py
@@ -1,17 +1,46 @@
 import os
 import numpy as np
+
 from mapdlgen import voxels_to_mapdl
 
 sample_dir = "data/samples/sample_000000"
 
-data = np.load(os.path.join(sample_dir, "voxels.npz"))
+data = np.load(
+    os.path.join(sample_dir, "voxels.npz")
+)
+
 voxels = data["voxels"]
 
 print("Voxel shape:", voxels.shape)
 
 mapdl = voxels_to_mapdl(voxels)
 
-print("MAPDL model created")
-
 print("Nodes:", mapdl.mesh.n_node)
 print("Elements:", mapdl.mesh.n_elem)
+
+print("Applying BCs...")
+
+mapdl.run("/SOLU")
+mapdl.antype("STATIC")
+
+# bottom fixed
+mapdl.nsel("S", "LOC", "Z", 0)
+mapdl.d("ALL", "ALL", 0)
+mapdl.allsel()
+
+# top displacement
+mapdl.nsel("S", "LOC", "Z", 10)
+mapdl.d("ALL", "UZ", -0.1)
+mapdl.allsel()
+
+print("Solving...")
+
+output = mapdl.solve()
+
+print(output)
+
+print("SOLVE COMPLETE")
+
+input("Press Enter to close MAPDL...")
+
+mapdl.exit()
