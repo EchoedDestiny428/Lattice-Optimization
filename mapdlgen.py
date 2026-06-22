6a6348ee598268e8211dd506ff3c3528775b054f
echoeddestiny428 <zacharyng428@gmail.com>
2026-06-22 09:11:30 +0800

    a

--- mapdlgen.py
+++ mapdlgen.py
@@ -1,4 +1,5 @@
 from ansys.mapdl.core import launch_mapdl
+import numpy as np
 
 BOX_SIZE = 10.0
 
@@ -6,62 +7,23 @@ BOX_SIZE = 10.0
 def voxels_to_mapdl(voxels):
 
     nx, ny, nz = voxels.shape
-    dx = BOX_SIZE / nx
-
-    print("Launching MAPDL...")
 
     mapdl = launch_mapdl()
-
     mapdl.clear()
     mapdl.prep7()
 
-    print("Defining element/material...")
-
+    # element + material
     mapdl.et(1, 185)
-
     mapdl.mp("EX", 1, 200e9)
     mapdl.mp("NUXY", 1, 0.3)
 
-    print("Creating nodes...")
+    # create block ONLY (fast)
+    mapdl.block(0, BOX_SIZE, 0, BOX_SIZE, 0, BOX_SIZE)
 
-    node_map = {}
-    nid = 1
+    # mesh block
+    mapdl.esize(BOX_SIZE / nx)
+    mapdl.vmesh("ALL")
 
-    for i in range(nx + 1):
-        for j in range(ny + 1):
-            for k in range(nz + 1):
-
-                mapdl.n(nid, i * dx, j * dx, k * dx)
-
-                node_map[(i, j, k)] = nid
-                nid += 1
-
-    print("Creating elements...")
-
-    elem_count = 0
-
-    for i in range(nx):
-        for j in range(ny):
-            for k in range(nz):
-
-                if not voxels[i, j, k]:
-                    continue
-
-                n = node_map
-
-                mapdl.e(
-                    n[(i, j, k)],
-                    n[(i + 1, j, k)],
-                    n[(i + 1, j + 1, k)],
-                    n[(i, j + 1, k)],
-                    n[(i, j, k + 1)],
-                    n[(i + 1, j, k + 1)],
-                    n[(i + 1, j + 1, k + 1)],
-                    n[(i, j + 1, k + 1)],
-                )
-
-                elem_count += 1
-
-    print(f"Created {elem_count} elements")
+    print(f"Created mesh from block")
 
     return mapdl
