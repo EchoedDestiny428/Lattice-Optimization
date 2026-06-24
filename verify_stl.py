43a036f78659b9ec49cb45a17c3d0f2450c9ec56
echoeddestiny428 <zacharyng428@gmail.com>
2026-06-24 09:25:46 +0800

    64 is too much

--- verify_stl.py
+++ verify_stl.py
@@ -10,7 +10,7 @@ from mapdlgen import voxels_to_mapdl
 
 STL_PATH = "test.stl"
 MODEL_WEIGHTS_PATH = "lattice_cnn.pth"
-RESOLUTION = 64
+RESOLUTION = 32
 UNIVERSAL_BOX_SIZE_MM = 10.0
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
